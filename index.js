997209271b39984cb3a235cba783b4872ed1aae7
Jonathan Derrough <jonathan.derrough@gmail.com>
2015-04-10 13:53:54 +0200

    Basic implementation.

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+
+var https = require('https');
+
+function GoogleContactsNoauth() {
+
+  if (!(this instanceof GoogleContactsNoauth)) {
+    return new GoogleContactsNoauth();
+  }
+
+  this.httpOptions = {
+    host: 'www.google.com',
+    port: 443,
+    path: '/m8/feeds/contacts/default/full?alt=json',
+    method: 'GET',
+    headers: {
+      'GData-Version': '3.0',
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  };
+
+}
+
+GoogleContactsNoauth.prototype.setAccessToken = function (accessToken) {
+
+  this.httpOptions.headers['Authorization'] = 'Bearer ' + accessToken;
+
 };
+
+GoogleContactsNoauth.prototype.getContacts = function (callback) {
+
+  var req = https.request(this.httpOptions, function(res) {
+
+    var output = '';
+    console.log(this.httpOptions.host + ':' + res.statusCode);
+    res.setEncoding('utf8');
+
+    res.on('data', function (chunk) {
+        output += chunk;
+    });
+
+    res.on('end', function() {
+        var obj = JSON.parse(output);
+        console.log(res.statusCode);
+        console.log(obj.feed.entry);
+        callback(null, obj.feed.entry);
+    });
+
+  });
+
+  req.on('error', function(err) {
+      console.log('error:', err.message);
+      callback(err);
+  });
+
+  req.end();
+
+};
+
+exports = module.exports = GoogleContactsNoauth;
--- test/test.js
+++ test/test.js
@@ -3,8 +3,9 @@ var assert = require('assert');
 var googleContactsNoauth = require('../');
 
 describe('google-contacts-noauth node module', function () {
-  it('must have at least one test', function () {
-    googleContactsNoauth();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+  it('must return an error if the access token was not set', function () {
+    googleContactsNoauth().getContacts(function (error) {
+      assert(error !== null, 'an error should have occured');
+    });
   });
 });
